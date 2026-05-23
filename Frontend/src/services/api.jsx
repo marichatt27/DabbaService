b486686c70d71171417be76fbd2bfbce4683014e
Srutakeerthi <srutakeerthivarkala@gmail.com>
2026-05-23 21:34:49 +0530

    commit

--- Frontend/src/services/api.jsx
+++ Frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://dabbaservice.onrender.com",
+  baseURL: "https://dabbaservice.onrender.com/api",
   withCredentials: true,
 });
 

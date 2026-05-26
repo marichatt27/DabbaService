98f394748a07844282f38d539f57edbeb0828618
Srutakeerthi <srutakeerthivarkala@gmail.com>
2026-05-26 17:29:52 +0530

    commit

--- Backend/middleware/authMiddleware.js
+++ Backend/middleware/authMiddleware.js
@@ -1,23 +1,25 @@
 const jwt = require("jsonwebtoken");
 
-const authMiddleware = (req, res, next) => {
+const protect = async (req, res, next) => {
   try {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
+    const token = req.cookies.token;
+
+    if (!token) {
       return res.status(401).json({
-        message: "No token provided",
+        message: "Not authorized",
       });
     }
 
-    const token = authHeader.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
     req.user = decoded;
+
     next();
   } catch (error) {
-    res.status(401).json({
-      message: "Invalid token",
+    return res.status(401).json({
+      message: "Token failed",
     });
   }
 };
 
-module.exports = authMiddleware;
+module.exports = protect;
